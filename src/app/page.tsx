a6f63552b8558206209aff99d89a787644a0ea76
rahul <raxhvl@users.noreply.github.com>
2024-08-09 13:33:29 +0000

    ✨ feat: Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,26 @@ export default function Home() {
       <section className="h-dvh p-6">
         <div className="h-full bg-primary rounded-3.5xl s:rounded-4xl flex flex-row p-6">
           <div className="basis-2/5">
+            <img src="/logo-flip.png " className="w-[200px] h-[200px]" />
+            <h2>Experience Coldplay Live: MUSIC of the SPHERES tour</h2>
+            <p>
+              Join Coldplay on their highly anticipated MUSIC of the SPHERES
+              tour! Experience a stunning performance in the magical city of
+              Lumina Falls, renowned for its shimmering skies and cascading
+              starlight. With a blend of heart-stirring anthems and mesmerizing
+              visuals, this night promises to be an unforgettable journey.
+              Secure your tickets now and let Coldplay's music guide you through
+              an extraordinary evening under the stars.
+            </p>
             <SignIn />
           </div>
-          <figure className="basis-3/5 flex flex-col">
+          <figure className="basis-3/5 flex flex-col relative">
+            <div className="absolute flex justify-center w-[75%] h-[75%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <img
+                src="/overlay.png"
+                className="object-contain animate-spin-slow"
+              />
+            </div>
             <img
               src="/coldplay.png"
               className="w-full object-contain min-h-0 h-full"
